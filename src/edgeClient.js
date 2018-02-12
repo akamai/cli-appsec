76fb4575001f4959838f09b6844e6d515e730173
Steven Boscarine <sboscari@akamai.com>
2018-02-12 16:18:49 -0500

    Integrate Arul's updates + add POST/PUT infrastructure + add update/create rule.

--- src/edgeClient.js
+++ src/edgeClient.js
@@ -1,20 +1,28 @@
 'use strict';
-let untildify = require("untildify");
-var EdgeGrid = require("edgeGrid");
-var logger = require("pino")({
-  level: process.env.LOG_LEVEL ? process.env.LOG_LEVEL : "error",
-  prettyPrint:true,
-  name:"EdgeClient"
+let untildify = require('untildify');
+var EdgeGrid = require('edgeGrid');
+var logger = require('pino')({
+  level: process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'error',
+  prettyPrint: true,
+  name: 'EdgeClient'
 });
 class Edge {
-  constructor(auth = {
-    path: "~/.edgerc",
-    section: "appsec",
-    debug: false,
-    default: true
-  }) {
+  constructor(
+    auth = {
+      path: '~/.edgerc',
+      section: 'appsec',
+      debug: false,
+      default: true
+    }
+  ) {
     if (auth.clientToken && auth.clientSecret && auth.accessToken && auth.host) {
-      this._edge = new EdgeGrid(auth.clientToken, auth.clientSecret, auth.accessToken, auth.host, auth.debug);
+      this._edge = new EdgeGrid(
+        auth.clientToken,
+        auth.clientSecret,
+        auth.accessToken,
+        auth.host,
+        auth.debug
+      );
     } else {
       this._edge = new EdgeGrid({
         path: untildify(auth.path),
@@ -25,28 +33,101 @@ class Edge {
   }
 
   get(requestUri) {
-
     let request = {
-      method: "GET",
+      method: 'GET',
       path: requestUri,
       followRedirect: false
     };
 
     return new Promise((resolve, reject) => {
       this._edge.auth(request);
-      this._edge.send(function (data, response) {
-        logger.debug("Requesting: "+request.path+"\nResponse from server: " + JSON.stringify(response));
+      this._edge.send(function(data, response) {
+        logger.debug(
+          'Requesting: ' + request.path + '\nResponse from server: ' + JSON.stringify(response)
+        );
         if (response && response.statusCode >= 200 && response.statusCode < 400) {
           resolve(JSON.parse(response.body));
-        } else if(!response) {
-          logger.info("Error response from server: "+JSON.stringify(data));
-          reject("Could not get data at this time.");
+        } else if (!response) {
+          logger.info('Error response from server: ' + JSON.stringify(data));
+          reject('Could not get data at this time.');
         } else {
-          logger.info("Error response from server: "+JSON.stringify(response));
-          try  {
+          logger.info('Error response from server: ' + JSON.stringify(response));
+          try {
             let errJson = JSON.parse(response.body);
             reject(errJson.detail);
-          } catch(err) {
+          } catch (err) {
+            reject(response.body);
+          }
+        }
+      });
+    });
+  }
+  post(requestUri, body) {
+    let request = {
+      method: 'POST',
+      path: requestUri,
+      followRedirect: false,
+      body: body
+    };
+
+    return new Promise((resolve, reject) => {
+      this._edge.auth(request);
+      this._edge.send(function(data, response) {
+        logger.debug(
+          'Posting: ' +
+            request.path +
+            '\nbody: ' +
+            body +
+            '\nResponse from server: ' +
+            JSON.stringify(response)
+        );
+        if (response && response.statusCode >= 200 && response.statusCode < 400) {
+          resolve(JSON.parse(response.body));
+        } else if (!response) {
+          logger.info('Error response from server: ' + JSON.stringify(data));
+          reject('Could not get data at this time.');
+        } else {
+          logger.info('Error response from server: ' + JSON.stringify(response));
+          try {
+            let errJson = JSON.parse(response.body);
+            reject(errJson.detail);
+          } catch (err) {
+            reject(response.body);
+          }
+        }
+      });
+    });
+  }
+  put(requestUri, body) {
+    let request = {
+      method: 'PUT',
+      path: requestUri,
+      followRedirect: false,
+      body: body
+    };
+
+    return new Promise((resolve, reject) => {
+      this._edge.auth(request);
+      this._edge.send(function(data, response) {
+        logger.debug(
+          'PUT call to : ' +
+            request.path +
+            '\nbody: ' +
+            body +
+            '\nResponse from server: ' +
+            JSON.stringify(response)
+        );
+        if (response && response.statusCode >= 200 && response.statusCode < 400) {
+          resolve(JSON.parse(response.body));
+        } else if (!response) {
+          logger.info('Error response from server: ' + JSON.stringify(data));
+          reject('Could not get data at this time.');
+        } else {
+          logger.info('Error response from server: ' + JSON.stringify(response));
+          try {
+            let errJson = JSON.parse(response.body);
+            reject(errJson.detail);
+          } catch (err) {
             reject(response.body);
           }
         }
