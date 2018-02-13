95c213b3360a16127ee277414be5758d654c64d4
Steven Boscarine <sboscari@akamai.com>
2018-02-13 16:24:24 -0500

    Fixes

--- src/crb.js
+++ src/crb.js
@@ -5,35 +5,31 @@ let Edge =
 let URIs = require('./constants').URIS;
 let logger = require('./constants').logger('CRBHandler');
 let ConfigResourcesReader = require('./configResourcesUtil').resourceUtil;
-let Config = require('./appSecConfig').AppSecConfig;
+// let Config = require('./appSecConfig').AppSecConfig;
 
 require('string-format');
-var fs = require('fs');
+let fs = require('fs');
 const CRB_TEMPLATE_PATH = __dirname + '/../templates/crbTemplate.json';
-const ENCODING = 'utf8';
+
 class CRBHandler {
   constructor(auth) {
-    this._edge = new Edge(auth);
-    this._configResourceReader = new ConfigResourcesReader(this._edge);
-    this._config = new Config();
+    // this._edge = new Edge(auth);
+    this._configResourceReader = new ConfigResourcesReader(new Edge(auth));
+    // this._config = new Config();
   }
 
   template() {
-    return fs.readFileSync(CRB_TEMPLATE_PATH, ENCODING);
+    return fs.readFileSync(CRB_TEMPLATE_PATH, 'utf8');
   }
 
   getAllRules(options) {
-    logger.debug('======>>> 1 ');
-    return this._config.config(options).then(configObj => {
-      logger.debug('======>>> 2 ' + JSON.stringify(configObj));
-      options.config = configObj.configId;
-      return this._configResourceReader.readResource(options.config, URIs.GET_CRB_ALL, []);
-    });
+    return this._configResourceReader.readResource(options.config, URIs.GET_CRB_ALL, []);
   }
 
   getRule(options) {
     return this._configResourceReader.readResource(options.config, URIs.GET_CRB, [options.ruleId]);
   }
+
   createRule(options) {
     return this._configResourceReader.postResource(
       options.config,
@@ -42,6 +38,7 @@ class CRBHandler {
       []
     );
   }
+
   updateRule(options) {
     return this._configResourceReader.putResource(options.config, URIs.GET_CRB, options.file, [
       options.ruleId
