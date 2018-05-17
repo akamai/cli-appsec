86dc3f16b63e93668e5bffd780ed7248ff0018e2
Arul <anedumar@akamai.com>
2018-05-17 11:57:37 -0400

    Fix for verbose version

--- src/clone.js
+++ src/clone.js
@@ -2,10 +2,12 @@
 
 let URIs = require('./constants').URIS;
 let Config = require('./configprovider').configProvider;
+let Version = require('./versionsprovider').versionProvider;
 
 class CloneHandler {
   constructor(options) {
     this._config = new Config(options);
+    this._version = new Version(options);
     this._options = options;
   }
 
@@ -13,6 +15,10 @@ class CloneHandler {
     let version = this._options['version'];
     let payload = { createFromVersion: version, ruleUpdate: false };
     return this._config.createResource(URIs.CLONE, [], payload);
+    return this._version.getVersionNumber().then(version => {
+      let payload = { createFromVersion: version, ruleUpdate: false };
+      return this._config.createResource(URIs.CLONE, [], payload);
+    });
   }
 }
 
