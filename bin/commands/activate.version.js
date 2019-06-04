da21449589c6f842c108bba56e40fbe1bd656c76
Tadhg Pearson <tpearson@akamai.com>
2019-06-04 17:37:16 -0400

    Add support for acknowledged invalid hosts field

--- bin/commands/activate.version.js
+++ bin/commands/activate.version.js
@@ -35,6 +35,12 @@ class ActivateVersionCommand {
         desc: 'The comma separated email ids to get notification.',
         group: 'Options:',
         required: true
+      })
+      .stringArray('--acknowledge-invalid-hosts <invalid hostnames>', {
+        desc:
+          'A comma separated list of domains or subdomains that are listed in this configuration as protected, but which cannot be protected for some reason (for example, they are not managed by Akamai). If a previous activation response required you to acknowledge a list of invalid hosts, you can do so here.',
+        group: 'Options:',
+        required: false
       });
   }
 
