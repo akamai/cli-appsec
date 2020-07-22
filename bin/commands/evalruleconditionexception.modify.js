97b7ced13f739f146f51bf86ead0d1ab17ef83fd
Alan Lin <alli@bos-mpqbg.145bw.corp.akamai.com>
2020-07-22 09:45:38 -0400

    SECKSD-6835 Fixing some typos

--- bin/commands/evalruleconditionexception.modify.js
+++ bin/commands/evalruleconditionexception.modify.js
@@ -4,7 +4,7 @@ let out = require('./lib/out');
 class ModifyEvalRuleConditionExceptionCommand {
   constructor() {
     this.flags = 'modify-eval-rule-condition-exception';
-    this.desc = '(Beta) Modify modify eval rule condition exception.';
+    this.desc = '(Beta) Update eval rule condition exception.';
     this.setup = this.setup.bind(this);
     this.run = this.run.bind(this);
   }
