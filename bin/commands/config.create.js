52b5ede68356680a83c0a3874021fb0e394734b8
Padmanabham Gali <pgali@akamai.com>
2020-09-23 16:23:46 -0400

    SECKSD-7697 CLI for create config and create policy

---
+++ bin/commands/config.create.js
@@ -0,0 +1,39 @@
+let Config = require('../../src/configprovider').configProvider;
+let out = require('./lib/out');
+
+class CreateConfigCommand {
+  constructor() {
+    this.flags = 'create-config';
+    this.desc = 'Create a new security config.';
+    this.setup = this.setup.bind(this);
+    this.run = this.run.bind(this);
+  }
+
+  setup(sywac) {
+    sywac.string('@<path>', {
+      desc: 'The input file path.',
+      group: 'Options:',
+      mustExist: true
+    });
+  }
+
+  run(options) {
+    //get args
+    const args = process.argv.slice(3, 5);
+
+    if (!args[0] || !args[0].startsWith('@')) {
+      throw 'Missing file name.';
+    }
+    options.file = args[0].replace('@', '');
+
+    out.print({
+      promise: new Config(options).createConfig(),
+      args: options,
+      success: (args, data) => {
+        return JSON.stringify(data);
+      }
+    });
+  }
+}
+
+module.exports = new CreateConfigCommand();
