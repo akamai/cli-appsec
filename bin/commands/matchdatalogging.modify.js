db7c237e05469989895bc07affe2d014fef2fee2
Andriy Movchan <amovchan@akamai.com>
2023-01-24 13:17:45 -0500

    SECKSD-17438: updated match data logging functionality

--- bin/commands/matchdatalogging.modify.js
+++ bin/commands/matchdatalogging.modify.js
@@ -24,8 +24,14 @@ class MatchDataLoggingCommand {
           "Version Number. It can also take the values 'PROD' or 'PRODUCTION' or 'STAGING'. If not provided, latest version is assumed.",
         group: 'Optional:',
         required: false
+      })
+      .check((argv, context) => {
+        if (!argv['@path'].startsWith('@')) {
+          return context.cliMessage("ERROR: Invalid file name, should start with '@'");
+        }
       });
   }
+
   run(options) {
     options.file = options['@path'].replace('@', '');
     out.print({
