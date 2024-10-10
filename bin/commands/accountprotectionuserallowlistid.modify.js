0bcc78ab4c573156d1845e8a85ae47eeee591326
dshekhar <dshekhar@akamai.com>
2024-10-10 08:40:36 -0400

    IPR-11574 adding file param for user-allow-list update

--- bin/commands/accountprotectionuserallowlistid.modify.js
+++ bin/commands/accountprotectionuserallowlistid.modify.js
@@ -12,6 +12,9 @@ class ModifyAccountProtectionUserAllowListIdCommand {
 
   setup(sywac) {
     sywac
+      .positional('<@path>', {
+        paramsDesc: 'The input file path.'
+      })
       .number('--config <id>', {
         desc: 'Configuration ID. Mandatory if you have more than one configuration.',
         group: 'Optional:',
@@ -22,9 +25,16 @@ class ModifyAccountProtectionUserAllowListIdCommand {
           "Version Number. It can also take the values 'PROD' or 'PRODUCTION' or 'STAGING'. If not provided, latest version is assumed.",
         group: 'Optional:',
         required: false
+      })
+      .check((argv, context) => {
+        if (!argv['@path'].startsWith('@')) {
+          return context.cliMessage("ERROR: Invalid file name, should start with '@'");
+        }
       });
   }
   run(options) {
+    options.file = options['@path'].replace('@', '');
+
     out.print({
       promise: new AccountProtectionAdvancedSettings(options).updateUserAllowListId(),
       args: options,
